8e425a88c69c63507901a9990af80bc5a423b680
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-12-23 00:46:22 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useState, createContext } from 'react';
 import './App.css';
 import SignIn from './components/signin';
 import { useUserInfoContext } from './components/userInfo.jsx';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import NotFound from './pages/404.jsx';
-import ChatHome from './pages/ChatHome.jsx';
 import Chat from './pages/Chat.jsx';
 
 function App() {
@@ -12,40 +9,8 @@ function App() {
   console.log(`isLogin : ${JSON.stringify(userInfo.current.isLogin)}`);
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              userInfo.current.isLogin ? (
-                // <ChatHome />
-                <Navigate replace to="/chat" />
-              ) : (
-                <Navigate replace to="/signin" />
-              )
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              userInfo.current.isLogin ? (
-                <Navigate replace to="/" />
-              ) : (
-                <SignIn />
-              )
-            }
-          />
-          <Route
-            path="/chat"
-            element={
-              userInfo.current.isLogin ? <Chat /> : <Navigate replace to="/" />
-            }
-          />
-          {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <SignIn className=`${userInfo.current.isLogin ? "hidden" : "visible"}` />
+      <Chat className=`${userInfo.current.isLogin ? "visible" : "hidden"} />
     </div>
   );
 }
